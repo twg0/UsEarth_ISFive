733486af417d8bed1649787864c0924f5d217c44
twg0 <fruitsjh@gmail.com>
2023-08-15 21:48:03 +0900

    feat: ChatRoom 구현

--- src/main/java/com/isfive/usearth/entity/ChatRoom.java
+++ src/main/java/com/isfive/usearth/entity/ChatRoom.java
@@ -1,4 +1,27 @@
 package com.isfive.usearth.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	String roomName;
+
+	@Column(nullable = false)
+	String description;
+
+	Integer memberCount;
+
+	// private List<ChatRoomAppend> chatRoomAppends;
 }
