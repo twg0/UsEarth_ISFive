9fe88ef3227485adfd4a3bab1fed197dcc4ddb83
eunhwa0308 <eunhwa0308@naver.com>
2023-08-16 03:42:30 +0900

    Feat : Post Entity 구현

---
+++ src/main/java/com/isfive/usearth/Post.java
@@ -0,0 +1,41 @@
+package com.isfive.usearth;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 128)
+    private String title;
+
+    private String content;
+    private int views;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "board_id")
+//    private Board board;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//    @OneToMany(mappedBy = "post")
+//    private List<Comment> comments = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "post")
+//    private List<PostLike> likes = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "post")
+//    private List<FileImage> images = new ArrayList<>();
+
+
+
+
+
+}
