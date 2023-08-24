e4984e27a2999573cc6d6b8a9da045e8e1203a92
OH CHANGMIN <lou0124@naver.com>
2023-08-24 17:25:59 +0900

    test: 댓글 생성 테스트

--- src/test/java/com/isfive/usearth/web/board/PostCommentControllerTest.java
+++ src/test/java/com/isfive/usearth/web/board/PostCommentControllerTest.java
@@ -3,11 +3,14 @@ package com.isfive.usearth.web.board;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isfive.usearth.domain.board.entity.Board;
 import com.isfive.usearth.domain.board.entity.Post;
+import com.isfive.usearth.domain.board.entity.PostComment;
 import com.isfive.usearth.domain.board.repository.BoardRepository;
+import com.isfive.usearth.domain.board.repository.PostCommentRepository;
 import com.isfive.usearth.domain.board.repository.PostRepository;
 import com.isfive.usearth.domain.member.entity.Member;
 import com.isfive.usearth.domain.member.repository.MemberRepository;
 import com.isfive.usearth.web.board.dto.PostCommentCreateRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -41,6 +47,8 @@ class PostCommentControllerTest {
 
     @Autowired MemberRepository memberRepository;
 
+    @Autowired PostCommentRepository postCommentRepository;
+
     @DisplayName("사용자는 댓글을 작성할 수 있다.")
     @Test
     void writeComment() throws Exception {
@@ -49,16 +57,17 @@ class PostCommentControllerTest {
                 .email("post writer")
                 .build();
 
-        Member member = Member.builder()
-                .email("comment writer")
+        Member other = Member.builder()
+                .email("other")
                 .build();
 
-        memberRepository.save(member);
+        memberRepository.save(writer);
+        memberRepository.save(other);
 
         Board board = Board.createBoard("게시판 제목", "게시판 요약");
         boardRepository.save(board);
 
-        Post post = Post.createPost(member, board, "title", "content");
+        Post post = Post.createPost(writer, board, "title", "content");
         postRepository.save(post);
         PostCommentCreateRequest request = new PostCommentCreateRequest("댓글입니다.");
         //when //then
@@ -69,5 +78,11 @@ class PostCommentControllerTest {
                 )
                 .andDo(MockMvcResultHandlers.print());
 
+        Post findPost = postRepository.findById(post.getId()).orElseThrow();
+        List<PostComment> all = postCommentRepository.findAll();
+
+        assertThat(findPost.getCommentCount()).isEqualTo(1);
+        assertThat(all.size()).isEqualTo(1);
     }
+
 }
