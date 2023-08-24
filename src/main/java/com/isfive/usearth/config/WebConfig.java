d4fab2c8b1576e7c04a9c05b9ff0cb0e66673477
twg0 <fruitsjh@gmail.com>
2023-08-25 01:39:12 +0900

    fix : WebConfig 위치 변경

--- src/main/java/com/isfive/usearth/config/WebConfig.java
+++ src/main/java/com/isfive/usearth/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.isfive.usearth.domain.board;
+package com.isfive.usearth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
