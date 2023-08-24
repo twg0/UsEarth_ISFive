2dfcef4cc7a5bedc40549bf975b95bf3d30113d0
twg0 <fruitsjh@gmail.com>
2023-08-25 01:33:53 +0900

    feat : exception 추가 - Conflict - FileProcessing - InvalidValue - CookieProcessing

---
+++ src/main/java/com/isfive/usearth/exception/FileProcessingException.java
@@ -0,0 +1,12 @@
+package com.isfive.usearth.exception;
+
+public class FileProcessingException extends BusinessException{
+
+	public FileProcessingException(ErrorCode errorCode, String detail) {
+		super(errorCode, detail);
+	}
+
+	public FileProcessingException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+}
