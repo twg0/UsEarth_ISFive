401a51d931b3f3c8f989fffae291663be3c0a263
twg0 <fruitsjh@gmail.com>
2023-08-25 01:35:25 +0900

    feat : 필요 에러코드 추가

--- src/main/java/com/isfive/usearth/exception/ErrorCode.java
+++ src/main/java/com/isfive/usearth/exception/ErrorCode.java
@@ -11,8 +11,13 @@ public enum ErrorCode {
 	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "P_001", "게시글을 찾을 수 없습니다."),
 
 	POST_WRITER_NOT_ALLOW(HttpStatus.BAD_REQUEST, "PW_001", "작성자는 해당 기능을 사용할 수 없습니다."),
+	MEMBER_CONFLICT(HttpStatus.CONFLICT, "M_002", "이미 등록된 사용자입니다."),
 
-	REFRESH_TOKEN_INVALID(HttpStatus.BAD_REQUEST, "RF_001", "유효하지 않은 토큰입니다.");
+	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "PA_001", "비밀번호가 일치하지 않습니다."),
+
+	BAD_REQUEST_COOKIE(HttpStatus.BAD_REQUEST, "CK_001", "쿠키가 존재하지 않습니다."),
+
+	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TK_001", "유효하지 않은 토큰입니다.");
 
 	ErrorCode(HttpStatus httpStatus, String code, String message) {
 		this.httpStatus = httpStatus;
