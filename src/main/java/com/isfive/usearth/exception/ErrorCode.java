d2edcb85a0c5e953e43f0e22dafd8cf9c5af5dbf
twg0 <fruitsjh@gmail.com>
2023-08-31 16:54:40 +0900

    feat : 사용자 권한 에러코드 구현

--- src/main/java/com/isfive/usearth/exception/ErrorCode.java
+++ src/main/java/com/isfive/usearth/exception/ErrorCode.java
@@ -18,7 +18,8 @@ public enum ErrorCode {
 
 	BAD_REQUEST_COOKIE(HttpStatus.BAD_REQUEST, "CK_001", "쿠키가 존재하지 않습니다."),
 
-	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TK_001", "유효하지 않은 토큰입니다.");
+	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TK_001", "유효하지 않은 토큰입니다."),
+	AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED,"AF_","권한이 없습니다.");
 
 	ErrorCode(HttpStatus httpStatus, String code, String message) {
 		this.httpStatus = httpStatus;
