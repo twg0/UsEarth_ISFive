561bebf0741f57c6a9071bb7c3a277d4f5ba6a8d
sy-k-98 <sb05220@naver.com>
2023-09-07 16:50:09 +0900

    feat : 유효성 검사 추가

--- src/main/java/com/isfive/usearth/web/project/dto/ProjectRegister.java
+++ src/main/java/com/isfive/usearth/web/project/dto/ProjectRegister.java
@@ -1,16 +1,17 @@
 package com.isfive.usearth.web.project.dto;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import com.isfive.usearth.domain.common.FileImage;
 import com.isfive.usearth.domain.common.Period;
 import com.isfive.usearth.domain.project.dto.ProjectCreate;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
@@ -18,13 +19,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProjectRegister {
 
+	@NotBlank(message = "프로젝트 제목을 입력해야 합니다.")
 	private String title;
+
+	@NotBlank(message = "프로젝트 요약을 입력해야 합니다.")
 	private String summary;
+
+	@NotBlank(message = "프로젝트 내용을 입력해야 합니다.")
 	private String story;
+
+	@NotNull(message = "프로젝트 목표금액을 입력해야 합니다.")
 	private Integer targetAmount;
+
+	@NotBlank(message = "프로젝트 시작 날짜를 입력해야 합니다.")
 	private String startDate;
+
+	@NotBlank(message = "프로젝트 종료 날짜를 입력해야 합니다.")
 	private String endDate;
+
+	@NotBlank(message = "메이커 이름을 입력해야 합니다. 메이커는 이미 등록된 메이커만 가능합니다.")
 	private String makerName;
+
+	@NotBlank(message = "프로젝트 해시태그를 입력해야 합니다. 예시) #상의 #하의 #반팔")
 	private String hashTag;
 
 	public ProjectCreate toService(FileImage file) {
