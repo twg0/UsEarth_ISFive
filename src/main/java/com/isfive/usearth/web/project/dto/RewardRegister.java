c719ccbda1cd0da5e98399f43ad49a7d1e2c01ca
sy-k-98 <sb05220@naver.com>
2023-09-12 09:17:24 +0900

    style : swagger example 수정

--- src/main/java/com/isfive/usearth/web/project/dto/RewardRegister.java
+++ src/main/java/com/isfive/usearth/web/project/dto/RewardRegister.java
@@ -37,11 +37,11 @@ public class RewardRegister {
 	@NotNull(message = "배송비를 입력해야 합니다.")
 	private Integer deliveryFee;
 
-	@Schema(example = "색상\": \"검정,흰색\", \"사이즈\": \"S,M,L\"")
+	@Schema(example = "{\"색상\": \"검정,빨강,흰색\",\"사이즈\": \"S,M,L\"}")
 	@NotBlank(message = "옵션명과 옵션 상세내용 입력해야 합니다. 예시) \"색상\": \"검정,흰색\", \"사이즈\": \"S,M,L\"")
 	private Map<String, String> options;
 
-	@Schema(example = "검정,S\": 100, \"검정,M\": 100, \"검정,L\": 5 등")
+	@Schema(example = "{\"검정,S\": 100,\"검정,M\": 100,\"검정,L\": 5}")
 	@NotBlank(message = "옵션상세내용과 초기 수량을 입력해야 합니다. 예시) \"검정,S\": 100, \"검정,M\": 100, \"검정,L\": 5 등")
 	private Map<String, Integer> optionStocks;
 
