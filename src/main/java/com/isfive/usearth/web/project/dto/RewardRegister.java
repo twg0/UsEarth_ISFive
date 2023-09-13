316a1293022ad27b8fab887a42284a7dd781a0e6
sy-k-98 <sb05220@naver.com>
2023-09-13 16:39:33 +0900

    fix : swagger 작동 오류 수정

--- src/main/java/com/isfive/usearth/web/project/dto/RewardRegister.java
+++ src/main/java/com/isfive/usearth/web/project/dto/RewardRegister.java
@@ -1,16 +1,16 @@
 package com.isfive.usearth.web.project.dto;
 
-import java.util.Map;
-
 import com.isfive.usearth.domain.project.dto.RewardCreate;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Getter
 @Builder
@@ -39,11 +39,11 @@ public class RewardRegister {
 	private Integer deliveryFee;
 
 	@Schema(example = "{\"색상\": \"검정,빨강,흰색\",\"사이즈\": \"S,M,L\"}")
-	@NotBlank(message = "옵션명과 옵션 상세내용 입력해야 합니다. 예시) \"색상\": \"검정,흰색\", \"사이즈\": \"S,M,L\"")
+	@NotEmpty(message = "옵션명과 옵션 상세내용 입력해야 합니다. 예시) \"색상\": \"검정,흰색\", \"사이즈\": \"S,M,L\"")
 	private Map<String, String> options;
 
-	@Schema(example = "{\"검정,S\": 100,\"검정,M\": 100,\"검정,L\": 5}")
-	@NotBlank(message = "옵션상세내용과 초기 수량을 입력해야 합니다. 예시) \"검정,S\": 100, \"검정,M\": 100, \"검정,L\": 5 등")
+	@Schema(example = "{\"검정,S\": 100,\"검정,M\": 100,\"검정,L\": 100}")
+	@NotEmpty(message = "옵션상세내용과 초기 수량을 입력해야 합니다. 예시) \"검정,S\": 100, \"검정,M\": 100, \"검정,L\": 100 등")
 	private Map<String, Integer> optionStocks;
 
 	public RewardCreate toService() {
